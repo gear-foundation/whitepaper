5171eaf421c17f7224883618fac397f48b2373a8
Eugene Kotsarev <gimmethenick@gmail.com>
2026-02-10 18:34:25 +0300

    Fix nested anchor hydration

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,17 +1,14 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
 export const baseOptions: BaseLayoutProps = {
   nav: {
-    title: 'Gear Whitepaper',
+    title: "Gear Whitepaper",
   },
+  githubUrl: "https://github.com/gear-tech/gear",
   links: [
     {
-      text: 'Gear-tech.io',
-      url: 'https://gear-tech.io',
-    },
-    {
-      text: 'GitHub',
-      url: 'https://github.com/gear-tech/gear',
+      text: "Gear Technologies",
+      url: "https://gear-tech.io",
     },
   ],
 };
